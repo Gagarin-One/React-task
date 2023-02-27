c9bf8e66daffa00bc4f9fc2f7959cbb5d491c01f
Gagarin-One <92833239+Gagarin-One@users.noreply.github.com>
2023-02-27 17:50:12 +0900

    prepare project for development

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+export default function MyApp ({ Component, pageProps }:any) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <style jsx global>
+        {`
+          body {
+            font-family: 'Nunito', Roboto, system-ui, Tahoma, sans-serif;
+          }
+        `}
+      </style>
+    </>
+  );
+};
